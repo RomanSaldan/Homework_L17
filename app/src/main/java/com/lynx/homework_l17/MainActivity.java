8a26e4007ef22541aeacd6740a58324e7dde8c6b
Roman Saldan <hell4lynx@gmail.com>
2015-07-14 23:00:49 +0300

    Description of my first commit on GitHub :)

---
+++ app/src/main/java/com/lynx/homework_l17/MainActivity.java
@@ -0,0 +1,17 @@
+package com.lynx.homework_l17;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        // TODO some code
+    }
+
+}
