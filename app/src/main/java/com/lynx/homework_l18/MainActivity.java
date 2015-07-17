c257191b6a59be8f2c8cf01654c74d0a8fb1c13e
Roman Saldan <hell4lynx@gmail.com>
2015-07-17 23:06:06 +0300

    Added custom zoom buttons (edited)

--- app/src/main/java/com/lynx/homework_l18/MainActivity.java
+++ app/src/main/java/com/lynx/homework_l18/MainActivity.java
@@ -1,51 +1,33 @@
 package com.lynx.homework_l18;
 
 import android.app.Activity;
-<<<<<<< HEAD
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
 import android.widget.ImageButton;
 
-import com.google.android.gms.maps.CameraUpdate;
 import com.google.android.gms.maps.CameraUpdateFactory;
-=======
-import android.location.LocationManager;
-import android.os.Bundle;
 
->>>>>>> origin/BranchOne
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.MapFragment;
 import com.google.android.gms.maps.OnMapReadyCallback;
 
-<<<<<<< HEAD
 public class MainActivity extends Activity implements OnMapReadyCallback, View.OnClickListener {
 
     private GoogleMap mGoogleMap;
     private ImageButton btnZoomIn;
     private ImageButton btnZoomOut;
-=======
-
-public class MainActivity extends Activity implements OnMapReadyCallback {
-
-    private GoogleMap mGoogleMap;
->>>>>>> origin/BranchOne
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        initMap();
-<<<<<<< HEAD
-
         btnZoomIn = (ImageButton) findViewById(R.id.btnZoomIn_AM);
         btnZoomOut = (ImageButton) findViewById(R.id.btnZoomOut_AM);
         btnZoomIn.setOnClickListener(this);
         btnZoomOut.setOnClickListener(this);
 
-=======
->>>>>>> origin/BranchOne
+        initMap();
     }
 
     private void initMap() {
@@ -57,7 +39,6 @@ public class MainActivity extends Activity implements OnMapReadyCallback {
     public void onMapReady(GoogleMap googleMap) {
         mGoogleMap = googleMap;
         if(mGoogleMap == null) return;
-<<<<<<< HEAD
         mGoogleMap.setMyLocationEnabled(true);
         mGoogleMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
     }
@@ -73,10 +54,5 @@ public class MainActivity extends Activity implements OnMapReadyCallback {
                 break;
         }
     }
-=======
-        mGoogleMap.getUiSettings().setZoomControlsEnabled(true);
-        mGoogleMap.setMyLocationEnabled(true);
-        mGoogleMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
-    }
->>>>>>> origin/BranchOne
+
 }
