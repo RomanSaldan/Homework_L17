4d18bc6221db24d651c0af9351f6aeced1d7e1a1
Roman Saldan <hell4lynx@gmail.com>
2015-07-18 00:07:21 +0300

    Added simple markers and location button

--- app/src/main/java/com/lynx/homework_l18/MainActivity.java
+++ app/src/main/java/com/lynx/homework_l18/MainActivity.java
@@ -10,29 +10,47 @@ import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.MapFragment;
 import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 
-public class MainActivity extends Activity implements OnMapReadyCallback, View.OnClickListener {
+public class MainActivity extends Activity implements OnMapReadyCallback, GoogleMap.OnMapClickListener, View.OnClickListener {
 
     private GoogleMap mGoogleMap;
-    private ImageButton btnZoomIn;
-    private ImageButton btnZoomOut;
+    private ImageButton btnZoomIn_AM;
+    private ImageButton btnZoomOut_AM;
+    private ImageButton btnLocation_AM;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        btnZoomIn = (ImageButton) findViewById(R.id.btnZoomIn_AM);
-        btnZoomOut = (ImageButton) findViewById(R.id.btnZoomOut_AM);
-        btnZoomIn.setOnClickListener(this);
-        btnZoomOut.setOnClickListener(this);
-
+        initUI();
         initMap();
+    }
+
+    private void initUI() {
+        btnZoomIn_AM = (ImageButton) findViewById(R.id.btnZoomIn_AM);
+        btnZoomOut_AM = (ImageButton) findViewById(R.id.btnZoomOut_AM);
+        btnLocation_AM = (ImageButton) findViewById(R.id.btnLocation_AM);
+        btnZoomIn_AM.setOnClickListener(this);
+        btnZoomOut_AM.setOnClickListener(this);
+        btnLocation_AM.setOnClickListener(this);
     }
 
     private void initMap() {
         MapFragment mapFragment = (MapFragment) getFragmentManager().findFragmentById(R.id.googleMapFragment);
         mapFragment.getMapAsync(this);
+    }
+
+    private MarkerOptions prepareMarker(LatLng _position) {
+        MarkerOptions marker = new MarkerOptions();
+        marker  .title("Marker title")
+                .position(_position)
+                .snippet("My custom marker")
+                .icon(BitmapDescriptorFactory.defaultMarker());
+        return marker;
     }
 
     @Override
@@ -41,6 +59,7 @@ public class MainActivity extends Activity implements OnMapReadyCallback, View.O
         if(mGoogleMap == null) return;
         mGoogleMap.setMyLocationEnabled(true);
         mGoogleMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
+        mGoogleMap.setOnMapClickListener(this);
     }
 
     @Override
@@ -52,7 +71,14 @@ public class MainActivity extends Activity implements OnMapReadyCallback, View.O
             case R.id.btnZoomOut_AM:
                 mGoogleMap.animateCamera(CameraUpdateFactory.zoomOut());
                 break;
+            case R.id.btnLocation_AM:
+
+                break;
         }
     }
 
+    @Override
+    public void onMapClick(LatLng latLng) {
+        mGoogleMap.addMarker(prepareMarker(latLng));
+    }
 }
